bb89044325bcd7245d1aced7a7d97b17066dbbe4
Vikas Patil <vikaspatil0021o+github@gmail.com>
2025-01-29 21:33:51 +0530

    next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
